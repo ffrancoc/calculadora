32644a76d0a4823f6f2c5da5d3617a3f5b75c0e9
Francisco <jose2fcurin@gmail.com>
2022-01-05 15:05:16 -0600

    Llamada de las funciones

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculadora
+
+![alt img](https://github.com/ffrancoc/calculadora/blob/main/ScreenShot_20220105150132.jpeg)
--- calc.js
+++ calc.js
@@ -1,5 +1,5 @@
 const readLineSync = require('readline-sync')
-const { Sum } = require('./operations')
+const { Sum, Subtraction, Multiplication, Division } = require('./operations')
 
 
 let option
@@ -13,35 +13,59 @@ while (option != '0') {
     console.log('4)Division')
     console.log('0)Exit')
 
-    option = readLineSync.question("Input your option: ")
+    option = readLineSync.question("\nInput your option: ")
 
     switch (option) {
         case '1':
-            console.log('Sum selected')
-            readLineSync.question('Put any key to continue...')
+            console.log('\nSum selected\n')
+
+            var number_1 = readLineSync.questionFloat('Input number one: ')
+            var number_2 = readLineSync.questionFloat('Input number two: ')
+            console.log(`\nThe operation result is: ${Sum(number_1, number_2)}`)
+
+            readLineSync.question('\nPut any key to continue...')
             console.clear()
             break
         case '2':
-            console.log('Subtraction selected')
-            readLineSync.question('Put any key to continue...')
+            console.log('\nSubtraction selected\n')
+
+            var number_1 = readLineSync.questionFloat('Input number one: ')
+            var number_2 = readLineSync.questionFloat('Input number two: ')
+            console.log(`\nThe operation result is: ${Subtraction(number_1, number_2)}`)
+
+            readLineSync.question('\nPut any key to continue...')
             console.clear()
             break;
 
         case '3':
-            console.log('Multiplication selected')
-            readLineSync.question('Put any key to continue...')
+            console.log('\nMultiplication selected\n')
+
+            var number_1 = readLineSync.questionFloat('Input number one: ')
+            var number_2 = readLineSync.questionFloat('Input number two: ')
+            console.log(`\nThe operation result is: ${Multiplication(number_1, number_2)}`)
+
+            readLineSync.question('\nPut any key to continue...')
             console.clear()
             break
 
         case '4':
-            console.log('Division selected')
-            readLineSync.question('Put any key to continue...')
+            console.log('\nDivision selected\n')
+
+            var number_1 = readLineSync.questionFloat('Input number one: ')
+            var number_2 = readLineSync.questionFloat('Input number two: ')
+            console.log(`\nThe operation result is: ${Division(number_1, number_2)}`)
+
+            readLineSync.question('\nPut any key to continue...')
             console.clear()
             break
         case '0':
             console.log('See you, leiter')
             console.clear()
             process.exit()
+        default:
+            console.log('\nInvalid Option')
+            readLineSync.question('Put any key to continue...')
+            console.clear()
 
     }
 
